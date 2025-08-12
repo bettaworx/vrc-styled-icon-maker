6d4bcc0a38b86119dc60173a4d2effb5a8877016
nekochanfood <74849003+nekochanfood@users.noreply.github.com>
2025-08-13 01:34:02 +0900

    update

--- .Tools/phosphor_run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set TYPE=bold
-
-node ./SVG2PNG.js ./.assets/phosphor-icons/SVGs/%TYPE%/ ./input_images_%TYPE%/
-python ./img_process.py -i ./input_images_%TYPE%/ -o ./../phosphor-icons/%TYPE%/
-
-pause
